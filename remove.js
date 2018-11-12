29779a42863226d006962872112bd2ef40822bea
Gabriel Makiewicz <makiewicz.gabriel@gmail.com>
2018-11-12 15:57:20 +0100

    hi4

--- remove.js
+++
@@ -1,7 +0,0 @@
-window.onload = function () {
-  var body = document.getElementsByTagName('body')[0];
-  if(body != 'undefined') {
-    body.removeChild(body.lastElementChild);
-    console.log("Removed last child");
-  }
-}
