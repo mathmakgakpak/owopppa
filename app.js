d257415daefdee1e365b6dd268269fca0845bb37
Gabriel <makiewicz.gabriel@gmail.com>
2018-11-22 14:56:18 +0100

    hi66

--- app.js
+++ app.js
@@ -3247,7 +3247,30 @@ _global.eventSys.once(_conf.EVENTS.misc.toolsRendered, function () {
     lastX = null;
     lastY = null;
     });
+    if (OWOP.player.rank == 3) {
+        var brDiamWin = OWOP.windowSys.addWindow(new OWOP.windowSys.class.window('Brush diameter', {}, function(win) {
+        win.container.title = 'Sets brush diameter. (duh)';
+        win.container.style.height = '16px';
+        win.container.style.overflow = 'hidden';
 
+        var brDiamElm = OWOP.util.mkHTML('span', { innerHTML: brDiameter });
+        win.addObj(brDiamElm);
+        var Rbar = OWOP.util.mkHTML('input', {
+            type: 'range', style: '-moz-appearance:none;-webkit-appearance:none;appearance:none;height:6px;outline:none;float:right;',
+            min: 2, max: 16,
+            value: brDiameter,
+            oninput: function() {
+                brDiameter = this.value;
+                brDiamElm.innerHTML = this.value;
+            }, ondblclick:function() {
+                this.value = 3; 
+                this.onchange();
+            }
+        });
+        win.addObj(Rbar);
+    }).move(945, 32));
+    }
+}));
 	
 //Text Tool
 OWOP.tool.addToolObject(new OWOP.tool.class("Text", OWOP.cursors.write, OWOP.fx.player.NONE, OWOP.RANK.USER, function(tool) {
